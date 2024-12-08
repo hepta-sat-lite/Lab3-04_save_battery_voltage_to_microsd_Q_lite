4e5a54782ca163e5a45cbd2a28c7887e1c8bfe64
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 06:21:08 +0000

    fix eps lib

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "mbed.h"
 #include "LITE_CDH.h"
 #include "LITE_EPS.h"
- 
+
 RawSerial pc(USBTX,USBRX,9600);
 
- 
+
  
 int main()
 {
 
-    float bt;
+    float batvol;
     char str[100];
     mkdir("/sd/mydir", 0777);
     FILE *fp = fopen("/sd/mydir/test.txt","w");
@@ -17,15 +17,15 @@ int main()
         error("Could not open file for write\r\n");
     }
     for(int i = 0; i < 10; i++) {
-       
-       
-       
+        
+
+
     }
     fclose(fp);
     fp = fopen("/sd/mydir/test.txt","r");
     for(int j = 0; j < 10; j++) {
         
-        
+
     }
     fclose(fp);
     pc.printf("Goodbye!!\r\n");
